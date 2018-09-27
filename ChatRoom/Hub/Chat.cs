21531ef05ece565ab53ddbb0e419328481051c1d
Zhenghui Yan <vwxyzh@gmail.com>
2018-09-27 11:07:00 +0800

    add code.

---
+++ ChatRoom/Hub/Chat.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Chat : Hub
+    {
+        // todo: 实现Broadcast和Echo
+    }
+}
